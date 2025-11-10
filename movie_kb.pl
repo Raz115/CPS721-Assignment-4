173fbaf2f011fd19759e9476d0439acf0c416e6f
Raz115 <razahmirza5@gmail.com>
2025-11-09 21:50:07 -0500

    Task 1 Done

--- movie_kb.pl
+++ movie_kb.pl
@@ -2,11 +2,91 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
-%%%%% NAME:
-%%%%% NAME:
+%%%%% NAME: Raza Hussain Mirza
+%%%%% NAME: Alvin Chan
+%%%%% NAME: Amitoz Banga
 
 % Put your movie KB in the section below
+% Movie 1
+releaseInfo('John Wick', 2014, 101).
+directedBy('John Wick', 'Chad Stahelski').
+actedIn('Keanu Reeves', 'John Wick', 'John Wick').
+actedIn('Michael Nyqvist', 'John Wick', 'Viggo Tarasov').
+movieGenre('John Wick', 'action').
+movieGenre('John Wick', 'thriller').
+
+% Movie 2
+releaseInfo('Jurassic Park', 1993, 127).
+directedBy('Jurassic Park', 'Steven Spielberg').
+actedIn('Sam Neill', 'Jurassic Park', 'Dr. Alan Grant').
+actedIn('Laura Dern', 'Jurassic Park', 'Dr. Ellie Sattler').
+movieGenre('Jurassic Park', 'sci-fi').
+movieGenre('Jurassic Park', 'adventure').
+
+% Movie 3
+releaseInfo('The Godfather', 1972, 175).
+directedBy('The Godfather', 'Francis Ford Coppola').
+actedIn('Marlon Brando', 'The Godfather', 'Vito Corleone').
+actedIn('Al Pacino', 'The Godfather', 'Michael Corleone').
+movieGenre('The Godfather', 'crime').
+movieGenre('The Godfather', 'drama').
+
+% Movie 4
+releaseInfo('The Conjuring', 2013, 112).
+directedBy('The Conjuring', 'James Wan').
+actedIn('Vera Farmiga', 'The Conjuring', 'Lorraine Warren').
+actedIn('Patrick Wilson', 'The Conjuring', 'Ed Warren').
+movieGenre('The Conjuring', 'horror').
+movieGenre('The Conjuring', 'thriller').
+
+% Movie 5
+releaseInfo('The Nun', 2018, 96).
+directedBy('The Nun', 'Corin Hardy').
+actedIn('Taissa Farmiga', 'The Nun', 'Sister Irene').
+actedIn('Demián Bichir', 'The Nun', 'Father Burke').
+movieGenre('The Nun', 'horror').
+movieGenre('The Nun', 'mystery').
+
+% Movie 6
+releaseInfo('Jumanji', 1995, 104).
+directedBy('Jumanji', 'Joe Johnston').
+actedIn('Robin Williams', 'Jumanji', 'Alan Parrish').
+actedIn('Kirsten Dunst', 'Jumanji', 'Judy Shepherd').
+movieGenre('Jumanji', 'adventure').
+movieGenre('Jumanji', 'fantasy').
+
+% Movie 7
+releaseInfo('Avengers: Endgame', 2019, 181).
+directedBy('Avengers: Endgame', 'Anthony Russo').
+directedBy('Avengers: Endgame', 'Joe Russo').
+actedIn('Robert Downey Jr.', 'Avengers: Endgame', 'Tony Stark').
+actedIn('Chris Evans', 'Avengers: Endgame', 'Steve Rogers').
+movieGenre('Avengers: Endgame', 'action').
+movieGenre('Avengers: Endgame', 'sci-fi').
+
+% Movie 8
+releaseInfo('The Exorcist', 1973, 122).
+directedBy('The Exorcist', 'William Friedkin').
+actedIn('Linda Blair', 'The Exorcist', 'Regan MacNeil').
+actedIn('Ellen Burstyn', 'The Exorcist', 'Chris MacNeil').
+movieGenre('The Exorcist', 'horror').
+movieGenre('The Exorcist', 'thriller').
+
+% Movie 9
+releaseInfo('Rush Hour', 1998, 98).
+directedBy('Rush Hour', 'Brett Ratner').
+actedIn('Jackie Chan', 'Rush Hour', 'Inspector Lee').
+actedIn('Chris Tucker', 'Rush Hour', 'Detective Carter').
+movieGenre('Rush Hour', 'action').
+movieGenre('Rush Hour', 'comedy').
+
+% Movie 10
+releaseInfo('Minecraft: The Movie', 2025, 105).
+directedBy('Minecraft: The Movie', 'Joaquim Dos Santos').
+actedIn('Jensen Ackles', 'Minecraft: The Movie', 'Steve').
+actedIn('Paola Núñez', 'Minecraft: The Movie', 'Alex').
+movieGenre('Minecraft: The Movie', 'adventure').
+movieGenre('Minecraft: The Movie', 'family').
 
 %%%%% SECTION: movie_database
 
